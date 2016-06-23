eb77d691bec1d77a4368012bffc36672a8d0a923
Barbara Gabriela <barbi.garza@gmail.com>
2016-06-23 13:35:09 +0200

    Get users with more karma

---
+++ users.py
@@ -0,0 +1,15 @@
+import csv
+
+import wrapper
+
+def get_top_users():
+    top_users = {}
+
+    with open('DATA/top_users.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            username = row['USERNAME']
+            karma = int(row['KARMA'])
+            top_users[username] = karma
+
+    return top_users
