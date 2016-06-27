fc52552283acc24a546e7701de64da25d13a6daf
Barbara Gabriela <barbi.garza@gmail.com>
2016-06-27 22:10:57 +0200

    Fix main

--- main.py
+++ main.py
@@ -30,26 +30,41 @@ def main():
             c_last_label_id = 1
             p_last_label_id = 1
             # Get users
-            top_users = users.get_top_users()
+            # top_users = users.get_top_users()
+            top_users = ['barbss', 'metalan', 'fadeblack13']
 
             # Loop through users and get the list of subreddits they comment and post
             for username in top_users:
                 # Create the network
                 print username
+                # subreddits = helper.get_subreddit_list(username)
                 subreddits, commented_subs, posts_subs = helper.get_subreddit_list(username)
+
                 # Combination of comments and posts
                 relationships = helper.add_relationship(relationships, subreddits)
                 last_label_id = helper.add_labels(labels, subreddits, last_label_id)
-                helper.write_pajek(labels, relationships, 'top_users_2')
+                helper.write_pajek(labels, relationships, 'top_users')
                 helper.write_json(labels, relationships, 'top_users')
                 # Comments
                 comments_relationships = helper.add_relationship(comments_relationships, commented_subs)
                 c_last_label_id = helper.add_labels(comments_labels, commented_subs, c_last_label_id)
-                helper.write_pajek(comments_labels, comments_relationships, 'users_comments')
+
                 # Posts
                 posts_relationships = helper.add_relationship(posts_relationships, posts_subs)
                 p_last_label_id = helper.add_labels(posts_labels, posts_subs, p_last_label_id)
-                helper.write_pajek(posts_labels, posts_relationships, 'users_posts')
+
+            # Testing
+            # helper.write_pajek(labels, relationships, 'test_users')
+            # helper.write_json(labels, relationships, 'test_users')
+
+            helper.write_pajek(labels, relationships, 'top_users')
+            helper.write_json(labels, relationships, 'top_users')
+
+            helper.write_pajek(comments_labels, comments_relationships, 'users_comments')
+            helper.write_json(comments_labels, comments_relationships, 'users_comments')
+
+            helper.write_pajek(posts_labels, posts_relationships, 'users_posts')
+            helper.write_json(comments_labels, comments_relationships, 'users_comments')
 
     # helper.get_subreddit_list_with_user()
 
